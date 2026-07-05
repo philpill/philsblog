1145fb673d443b758fd8db7d606a8678485b9647
philpill <github@philpill.net>
2026-07-05 13:08:54 +0100

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import alpinejs from '@astrojs/alpinejs';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server', // Enables the proxy to run on the fly
+
+  adapter: netlify(),
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [alpinejs()]
+});
